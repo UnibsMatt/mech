6fbf07067ba840c2bec8bccc2a52d444b9f62640
mattia.federici <mattia.federici@orobix.com>
2023-12-12 10:22:44 +0100

    feat: add supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import {useEffect, useState} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import CustomBar from "./components/navbar/NavBar";
@@ -8,6 +8,8 @@ import {createTheme, ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Login from "./pages/Login";
 import './App.css';
+import {supabase} from "./client";
+import Button from "@mui/material/Button";
 
 
 const darkTheme = createTheme({
@@ -16,11 +18,41 @@ const darkTheme = createTheme({
     },
 });
 
+
+async function login() {
+    const {data, error} = await supabase.auth.signInWithPassword({
+        email: 'gianni@sperti.com',
+        password: 'masdmasd',
+    })
+    console.log(data)
+    return data
+
+}
+
 function App() {
+
+    const [user, setUser] = useState("");
+
+    useEffect(() => {
+            const fetchUser = async () => {
+                const {user, session} = await login()
+                if (user !== null) {
+                    setUser(user.email != null ? user.email : "");
+                }
+            }
+
+            fetchUser();
+
+
+        },
+        []
+    )
+
 
     return (
         <ThemeProvider theme={darkTheme}>
             <CssBaseline/>
+            <Button onClick={login}> Press me {user}</Button>
             <BrowserRouter>
                 <CustomBar/>
                 <Routes>
